f644d744b30776b90fc78aa99938fbbff1b827f8
Vinicius Dias Oliveira <vini2208@gmail.com>
2025-03-08 11:03:03 -0300

    Project creation

---
+++ src/main/java/com/dozenflow/dozenflow_be/DozenflowBeApplication.java
@@ -0,0 +1,13 @@
+package com.dozenflow.dozenflow_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DozenflowBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DozenflowBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dozenflow/dozenflow_be/DozenflowBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dozenflow.dozenflow_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DozenflowBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
